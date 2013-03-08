b32cde22b1a0207927e018f35e081090b328d39d
Cannon Matthews <cannonmatthews@uchciago.edu>
2013-03-08 02:17:21 -0600

    Updated main to integrate with yacc parser
    Moved the drawing stuff into a sperate fucntion, added a loop around
    that. read in from a a file, etc

--- newt.c
+++ newt.c
@@ -6,8 +6,15 @@
 #include <float.h>
 #include <math.h> 
 
-complex f(complex z); 
+#include "expression.h" 
+#include "expression_parse.tab.h" 
 
+expression_list *expressions; 
+extern int yyparse(); 
+extern FILE *yyin; 
+//complex f(complex z); 
+
+void drawFractal(char *filename , expression_t * expr) ; 
 
 const int WIDTH = 2048, HEIGHT = 2048; // actual image size in pixels 
 
@@ -16,18 +23,41 @@ const double SCALED_X_MAX = 1.0;
 const double SCALED_X_MIN = -1.0;
 const double SCALED_Y_MAX = 1.0;
 const double SCALED_Y_MIN = -1.0;
-
-
+double SCALED_WIDTH; 
+double SCALED_HEIGHT; 
 
 const int MAX_ITERATIONS = 200;
 const double EPSILON = 1e-3; // max error
 
 int main(int argc, char **argv) { 
-    const double SCALED_WIDTH = SCALED_X_MAX - SCALED_X_MIN ;
-    const double SCALED_HEIGHT = SCALED_Y_MAX - SCALED_Y_MIN ; 
+    SCALED_WIDTH = SCALED_X_MAX - SCALED_X_MIN ;
+    SCALED_HEIGHT = SCALED_Y_MAX - SCALED_Y_MIN ; 
 
-    FILE * image = fopen("newton.ppm", "w") ; 
-    // PPM file header //
+    yyin = fopen("functions.txt", "r" ) ; 
+    expressions = new_expression_list(); 
+    yyparse(); 
+    expression_t *expression =  expression_list_pop(expressions);
+    printf(" expression is at %p \n" , expression ) ; 
+    int filenum= 0 ; 
+    while( expression  != NULL ) { 
+        char *filename ; 
+        asprintf(&filename, "fractal-%3d.ppm", filenum ) ; 
+        puts(filename) ; 
+        drawFractal(filename, expression) ;
+        free((void*) filename) ;
+        free_expression(expression); 
+
+        expression = expression_list_pop(expressions) ; 
+        filenum++; 
+    }
+    free_expression_list(expressions ) ;
+    exit(0); 
+
+}
+
+void drawFractal(char *filename, expression_t *expr) { 
+    fprintf(stderr,"printing fractal" ) ;
+    FILE *image = fopen(filename, "w") ; 
     fprintf(image, "P3\n"); // color pixmap, ASCII
     fprintf(image, "%d %d\n" , WIDTH , HEIGHT ) ; 
     fprintf(image, "%d\n", 255) ; // 255 = max value for color
@@ -44,9 +74,9 @@ int main(int argc, char **argv) {
                 complex h=sqrt(DBL_EPSILON) + sqrt(DBL_EPSILON)*I; // base h on our floating point percison 
                 complex volatile zph = z + h; // volatile to prevent an optimizing complier from removing the following calculations at complile time 
                 complex dz = zph - z; 
-                complex slope = (f(zph) - f(z))/dz; // f'(z) 
+                complex slope = (eval_expression(expr,zph) - eval_expression(expr,z))/dz; // f'(z) 
 
-                complex z0 = z - f(z)/slope     ;       // newton iteration 
+                complex z0 = z - eval_expression(expr,z)/slope     ;       // newton iteration 
                 if ( cabs(z0 - z) < EPSILON){ // when we get close enough break, base the color on the number of iterations it took. 
                     break; 
                 }
@@ -60,10 +90,4 @@ int main(int argc, char **argv) {
        fprintf(image, "\n") ; 
     }
     fclose(image) ; 
-
-    exit(0); 
-
-}
-complex f(complex z ) { 
-    return cpow(z,4)-cpow(z,3)-1.0 ;
-}
+} 
